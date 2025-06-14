e6ace04a6dbc73a22653210983eaa6eee5d80308
Aditya <2005akjha@gmail.com>
2025-06-14 13:08:54 +0530

    dependencies issuse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
      
-      
       <Hero />
-  
       <HowItWorks />
       <Features />
       <CTA />
